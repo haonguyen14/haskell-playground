757ad7b66f75c00d7e4a330561d8a8f68caef41d
Hao Nguyen <hao.nguyen1214@gmail.com>
2025-12-21 09:52:07 -0800

    further improvement

--- redis/lib/Control.hs
+++ redis/lib/Control.hs
@@ -1,4 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use lambda-case" #-}
 
 module Control
   ( execute,
@@ -7,17 +10,44 @@ where
 
 import CommandHandlers (KVOp, getAllH, getH, getKV, ping, setH, setKV)
 import qualified Data.ByteString.Char8 as BS
+import qualified Data.HashMap.Strict as HashMap
 import Resp (Value (..), bulkString, parseResp)
 import Text.Parsec
 
-getCommandHandler :: BS.ByteString -> [Value] -> KVOp Value
-getCommandHandler "PING" [msg] = ping msg
-getCommandHandler "SET" [key, value] = setKV key value
-getCommandHandler "GET" [key] = getKV key
-getCommandHandler "HSET" [tbl, key, value] = setH tbl key value
-getCommandHandler "HGET" [tbl, key] = getH tbl key
-getCommandHandler "HGETALL" [tbl] = toResp <$> getAllH tbl
-getCommandHandler cmd _ = error $ "ERR unknown command '" ++ BS.unpack cmd ++ "'"
+commandHandlers :: HashMap.HashMap BS.ByteString ([Value] -> KVOp Value)
+commandHandlers =
+  HashMap.fromList
+    [ ( "PING",
+        \args -> case args of
+          [msg] -> ping msg
+          _ -> error "ERR wrong number of arguments for 'PING' command"
+      ),
+      ( "SET",
+        \args -> case args of
+          [key, value] -> setKV key value
+          _ -> error "ERR wrong number of arguments for 'SET' command"
+      ),
+      ( "GET",
+        \args -> case args of
+          [key] -> getKV key
+          _ -> error "ERR wrong number of arguments for 'GET' command"
+      ),
+      ( "HSET",
+        \args -> case args of
+          [tbl, key, value] -> setH tbl key value
+          _ -> error "ERR wrong number of arguments for 'HSET' command"
+      ),
+      ( "HGET",
+        \args -> case args of
+          [tbl, key] -> getH tbl key
+          _ -> error "ERR wrong number of arguments for 'HGET' command"
+      ),
+      ( "HGETALL",
+        \args -> case args of
+          [tbl] -> toResp <$> getAllH tbl
+          _ -> error "ERR wrong number of arguments for 'HGETALL' command"
+      )
+    ]
 
 parseInput :: BS.ByteString -> KVOp (BS.ByteString, [Value])
 parseInput s = case parse parseResp "" s of
@@ -28,7 +58,7 @@ parseInput s = case parse parseResp "" s of
 execute :: BS.ByteString -> KVOp Value
 execute s = do
   (commandName, args) <- parseInput s
-  getCommandHandler commandName args
+  HashMap.lookupDefault (\_ -> error "Unknown command") commandName commandHandlers args
 
 toResp :: [(BS.ByteString, Value)] -> Value
 toResp xs = Array (fromIntegral $ 2 * length xs) $ concatMap (\(k, v) -> [bulkString k, v]) xs
